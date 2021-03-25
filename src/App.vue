161fa111b067ba1c1b3e78de557e2f14a18ca5f7
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-03-25 13:25:17 +0100

    Dynamic data display works

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,58 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Something works"/>
+    <line-chart  v-if="loaded" :chartdata="chartdata" :options="options" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import axios from "axios"
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
-  mounted(){
-    return (console.log("Mounted!"))
+  data:()=>({
+    loaded: false,
+    chartdata:null,
+    countrys:[],
+    options:{
+        responsive: true,
+        maintainAspectRatio: false
+        }
+  }),
+  async mounted (){
+    this.loaded = false;
+    axios.get('https://covid-api.mmediagroup.fr/v1/history?status=Confirmed&continent=Europe')
+    .then((res)=>{
+      const covidData = res.data;
+      this.chartdata ={
+        labels: [...(Object.keys(covidData.Albania.All.dates)).reverse()],
+        datasets:[
+          {
+            label:covidData.Albania.All.country,
+            backgroundColor: "#00aa00",
+            data:(Object.values(covidData.Albania.All.dates)).reverse()           
+          },
+          {
+            label:covidData.Poland.All.country,
+            backgroundColor: "#ff0000",
+            data:(Object.values(covidData.Poland.All.dates)).reverse()
+          }
+        ]
+      };
+      console.log(covidData.Poland.All)
+      this.loaded=true;
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+    
+
   }
 }
 </script>
@@ -29,3 +67,19 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+data: ()=>({
+      chartdata:{
+          labels:["One","Two"],
+          dataset:[
+              {
+                label:'Data One',
+                data:[111,123]
+                }
+            ]
+      },
+      options:{
+        
+    }
+  }),
+  //TODO: map all the countrys to datasets and label
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import LineChart from "./components/LineChart.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component("line-chart", LineChart);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
